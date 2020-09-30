190f9c5f28563d822ab176edaa02736289eaff7d
Allison Ricker <alliricker@gmail.com>
2020-09-30 22:25:07 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -11,3 +11,11 @@ require 'pry'
 def get_the_keys(groceries)
   groceries.keys
 end
+
+def get_the_values(groceries)
+  groceries.values.flatten
+end
+
+def get_the_min(groceries)
+groceries.values.min
+end 
